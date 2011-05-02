54525a3b487ae01eb4016e570afad4873970695c
Chris Pickel <sfiera@gmail.com>
2011-05-02 00:24:08 -0400

    Add "ctx.platform()" function.

--- platforms.py
+++ platforms.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
-from waflib.Utils import unversioned_sys_platform
+import sys
+from waflib.Configure import conf
+from waflib.Utils import to_list, unversioned_sys_platform
 
 def options(opt):
     opt.load("platform_%s" % unversioned_sys_platform())
 
 def configure(cnf):
     cnf.load("platform_%s" % unversioned_sys_platform())
+
+@conf
+def platform(ctx, target, platform, **kwds):
+    target = ctx.get_tgen_by_name(target)
+    if unversioned_sys_platform() not in to_list(platform):
+        return
+    for key, val in kwds.items():
+        setattr(target, key, to_list(getattr(target, key, [])))
+        getattr(target, key).extend(to_list(val))
