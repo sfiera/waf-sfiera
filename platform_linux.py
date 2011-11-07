87285dd85c2333014188888218dca2eb5db18c03
Chris Pickel <sfiera@sfzmail.com>
2011-11-07 02:31:58 -0500

    Ignore {universal,universal32} on Linux.

--- platform_linux.py
+++ platform_linux.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+
+
+@feature("universal")
+@before_method("process_source")
+def build_universal(self):
+    """Ignore universal feature."""
+
+
+@feature("universal32")
+@before_method("process_source")
+def build_universal(self):
+    """Ignore universal32 feature."""
